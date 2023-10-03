66d63dfa24d908c60fbc20ae61d4300dec2e479f
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-10-03 14:42:25 -0300

    Foi aprendido sobre Query params = ?nome=NodeJS e Route params = /curso/2

--- index.js
+++ index.js
@@ -3,15 +3,34 @@ const express = require ('express');
 
 const server = express();
 
+
+
+//Request Body = {nome:'Nodejs',tipo:'Backend'}
+ 
+
 //localhost:3000/curso
+//Query params = ?nome=NodeJS
+/*
 server.get('/curso', (req,res) => {
-    //console.log('ACESSOU A ROTA!!!');
-    //return res.send('HELLO WORLD!');
+    const nome = req.query.nome;
+    const aluno = 'Antonio';
 
     return  res.json({
-        curso:'Node JS',
-        aluno: 'Antonio'
+        curso:`Aprendendo ${nome}`,
+        aluno:`Aluno ${aluno}`,
+        
+    })
+});*/
+
+
+//Route params = /curso/2
+server.get('/curso/:id', (req,res) => {
+    const id = req.params.id;
+    
+
+    return  res.json({
+        curso:`Id do curso: ${id}`,
+       
     })
 });
-
 server.listen(3000);
